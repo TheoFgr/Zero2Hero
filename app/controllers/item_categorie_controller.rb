5bed3253f2394da79dfd23a4910bbf1e8cfad627
raphael <raphael.bichet@yahoo.com>
2022-02-22 11:51:51 +0100

    creation model cetagories et table de jointure item_categories

---
+++ app/controllers/item_categorie_controller.rb
@@ -0,0 +1,8 @@
+class ItemCategorieController < ApplicationController
+
+  def id_categorie
+  end
+
+  def id_item
+  end
+end
---
+++ test/controllers/item_categorie_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ItemCategorieControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
