c92c4064d5f928362086917ad262cefe94eca57b
theo faugere <theo.faugere3@gmail.com>
2022-02-22 15:25:07 +0100

    Routes 2.0

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,23 @@
+class BookingsController < ApplicationController
+  def index
+    @booking = Booking.new
+  end
+  def show
+    @booking = Booking.find(params[:id])
+  end
+  def new
+    @booking = Booking.new
+  end
+  def create
+    @booking = Booking.new(params[:booking])
+    if @booking.save
+      redirect_to user_item_bookings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  params.require(:booking).permit(:user_id, :item_id)
+end
