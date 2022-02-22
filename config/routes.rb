a3c94cfe70a23d975ddad74fbb044725a65a64eb
raphael <raphael.bichet@yahoo.com>
2022-02-22 16:12:41 +0100

    ajout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'items#index'
+
+  resources :users do
+    resources :items, expect: [:index] do
+      resources :bookings
+    end
+  end
 end
