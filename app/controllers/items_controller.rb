f8e447bddc77d437021caedd106dfba6dc7bc7ba
theo faugere <theo.faugere3@gmail.com>
2022-02-24 00:12:19 +0100

    new nav-bar fonctionnel

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,25 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @user = current_user
+    @item = Item.new(params_item)
+    @item.user = @user
+    if @item.save
+      redirect_to user_item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_item
+    params.require(:item).permit(:name, :description, :price)
+  end
 end
